62c331aa77b9cdbb7672f3fa786dfaef54550dc4
crow <70668775+jwcrow@users.noreply.github.com>
2020-10-07 13:06:51 -0700

    Initial Commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,35 @@
+/*
+ * The AddingMachine program implements an application that
+ * adds and subtracts like a calculator.
+ *
+ * @author  Jenny Wong
+ * @version 1.0
+ * @since   2020-10-07
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+    }
+
+    public void add (int value) {
+    }
+
+    public void subtract (int value) {
+    }
+
+    public String toString () {
+        return "";
+    }
+
+    public void clear() {
+    }
+}
